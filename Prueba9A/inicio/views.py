7d35865bb2b91c84738996620a25e77b5fb737f0
Zellenson <utm210081ti@ut-morelia.edu.mx>
2024-10-14 12:42:08 -0600

    Si llega o ne

--- Prueba9A/inicio/views.py
+++ Prueba9A/inicio/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 def principal(request):
     return render(request, "inicio/principal.html")
 
+def registrar(request):
+    return render(request, "inicio/registrar.html")
+
 # Create your views here.
